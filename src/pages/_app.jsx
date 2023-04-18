e08e9ba06f23890043702669aefffb2d7e8a291a
natalia <nata774e@stud.kea.dk>
2023-04-18 14:48:24 +0200

    linked product id

---
+++ src/pages/_app.jsx
@@ -0,0 +1,22 @@
+import "@/styles/globals.css";
+import Layout from "@/components/Layout";
+import App from "next/app";
+
+export default function MyApp({ Component, pageProps, navData }) {
+  return (
+    <>
+      <Layout navData={navData}>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
+}
+
+MyApp.getInitialProps = async (appContext) => {
+  // Provide the appContext, in order to do 404's
+  const appProps = await App.getInitialProps(appContext);
+  const res = await fetch("https://kea-alt-del.dk/t7/api/products/");
+  const navData = await res.json();
+  console.log(navData);
+  return { ...appProps, navData };
+};
---
+++ src/pages/products/[id].jsx
@@ -0,0 +1,57 @@
+import Head from "next/head";
+
+export default function Product({ data }) {
+  return (
+    <>
+      <Head>
+        <title>{data.productdisplayname}</title>
+      </Head>
+      <h1>{data.productdisplayname}</h1>
+      <p>Gender: {data.gender}</p>
+      <p>Category: {data.category}</p>
+      <p>Subcategory: {data.subcategory}</p>
+      <p>Articletype: {data.articletype}</p>
+      <p>Season: {data.season}</p>
+      <p>Production year: {data.productionyear}</p>
+      <p>Style: {data.usagetype}</p>
+      <p>Price: {data.price}</p>
+      {/* <p>{data.discount}</p> */}
+      <p>Brand: {data.brandname}</p>
+    </>
+  );
+}
+
+export async function getStaticProps(context) {
+  const id = context.params.id;
+  const api = "https://kea-alt-del.dk/t7/api/products/" + id;
+  const res = await fetch(api);
+  // If no data - no page (404)
+  if (res.status != 200) {
+    return {
+      notFound: true,
+    };
+  }
+  const data = await res.json();
+
+  return {
+    props: {
+      data: data,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const api = "https://kea-alt-del.dk/t7/api/products/";
+  const res = await fetch(api);
+  const data = await res.json();
+
+  const paths = data.map((object) => {
+    console.log(object.id);
+    return { params: { id: object.id.toString() } };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
